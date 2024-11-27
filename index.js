994e6d5ac2d1acca13b56afdcce1dce1ae5c34eb
Corey P <cparsons0730@yahoo.com>
2024-11-26 20:49:37 -0600

    Added contents to index, generateMarkdown and package.json

---
+++ index.js
@@ -0,0 +1,17 @@
+// TODO: Include packages needed for this application
+import inquirer from 'inquirer';
+import fs from 'fs';
+import colors from 'colors';
+import generateMarkdown from './generateMarkdown.js';
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
