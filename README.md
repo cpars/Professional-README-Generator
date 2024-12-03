c01ae92d91d9172353d6edf08d1f4017c51840ef
Corey P <cparsons0730@yahoo.com>
2024-12-02 18:29:15 -0600

    Updated the code

---
+++ README.md
@@ -0,0 +1,41 @@
+
+ # hello
+
+https://img.shields.io/badge/License-MIT-yellow.svg
+
+## Description
+this is my description
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+```
+these are the install instructions
+```
+
+## Usage
+these are the usage instructions
+
+
+    ## License
+    This project is licensed under the [MIT](https://opensource.org/licenses/MIT) license.
+
+## Contributing
+these are the guidelines
+
+## Tests
+```
+thes are the tests
+```
+
+## Questions
+For any questions, feel free to contact me:
+- GitHub: [cparsonssssss](https://github.com/cparsonssssss)
+- Email: sjdilsndf@gmail.com
+
--- index.js
+++ index.js
@@ -5,13 +5,73 @@ import colors from 'colors';
 import generateMarkdown from './generateMarkdown.js';
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+      type: "input",
+      name: "title",
+      message: "What is the title of your project?",
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "Provide a short description of your project:",
+    },
+    {
+      type: "input",
+      name: "installation",
+      message: "What are the installation instructions?",
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: "What are the usage instructions?",
+    },
+    {
+      type: "list",
+      name: "license",
+      message: "Choose a license for your project:",
+      choices: ["MIT", "GPLv3", "Apache 2.0", "BSD 3-Clause", "None"],
+    },
+    {
+      type: "input",
+      name: "contributing",
+      message: "What are the contribution guidelines?",
+    },
+    {
+      type: "input",
+      name: "tests",
+      message: "What are the test instructions?",
+    },
+    {
+      type: "input",
+      name: "github",
+      message: "What is your GitHub username?",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "What is your email address?",
+    },
+  ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data){
+fs.writeFile(fileName, data , (err) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    console.log("File created!".green);
+});
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((answers) => {
+        const readmeContent = generateMarkdown(answers);
+        writeToFile("README.md", readmeContent);
+    });
+}
 
 // Function call to initialize app
 init();
